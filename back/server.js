ee096dc88b37026b1794de55c759c49b502a1fdb
maher fadhlaoui <meherfadhlaoui@gmail.com>
2024-08-10 17:29:07 +0100

    Conteneurisation de l application

--- back/server.js
+++ back/server.js
@@ -6,14 +6,21 @@ const cors = require("cors");
 app.use(bodyParser.json());
 app.use(cors());
 
-let items = [];
+let items = [
+  { id: 1, name: "item 1", description: "description item 1" },
+  { id: 2, name: "item 2", description: "description item 2" },
+];
+
+// Used to auto increment the item ID
+let index = items.length;
 
 app.get("/api/items", (req, res) => {
   res.json(items);
 });
 
 app.post("/api/items", (req, res) => {
-  const newItem = req.body;
+  index++;
+  const newItem = { ...req.body, id: index };
   items.push(newItem);
   res.status(201).json(newItem);
 });
